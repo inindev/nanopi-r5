7aa08b83721f3ee23b55f277bf28bb7a31e5b9ce
John Clark <inindev@gmail.com>
2023-07-25 03:17:42 +0000

    option to copy u-boot bins to debian build cache
    Signed-off-by: John Clark <inindev@gmail.com>

--- uboot/make_uboot.sh
+++ uboot/make_uboot.sh
@@ -3,21 +3,25 @@
 set -e
 
 # script exit codes:
-#   5: invalid file hash
+#   1: missing utility
 
 main() {
     local utag='v2023.04'
+    local branch='2023.04'
     local atf_file='../rkbin/rk3568_bl31_v1.28.elf'
     local tpl_file='../rkbin/rk3568_ddr_1560MHz_v1.15.bin'
 
-    if [ '_clean' = "_$1" ]; then
-        rm -f u-boot/simple-bin.fit.*
-        make -C u-boot distclean
-        git -C u-boot clean -f
-        git -C u-boot checkout master
-        git -C u-boot branch -D $utag 2>/dev/null || true
-        git -C u-boot pull --ff-only
+    if is_param 'clean' "$@"; then
         rm -f *.img *.itb
+        if [ -d u-boot ]; then
+            rm -f u-boot/simple-bin.fit.*
+            make -C u-boot distclean
+            git -C u-boot clean -f
+            git -C u-boot checkout master
+            git -C u-boot branch -D "$branch" 2>/dev/null || true
+            git -C u-boot pull --ff-only
+        fi
+        echo '\nclean complete\n'
         exit 0
     fi
 
@@ -28,34 +32,37 @@ main() {
         git -C u-boot fetch --tags
     fi
 
-    if ! git -C u-boot branch | grep -q $utag; then
-        git -C u-boot checkout -b $utag $utag
+    if ! git -C u-boot branch | grep -q "$branch"; then
+        git -C u-boot checkout -b "$branch" "$utag"
 
         cherry_pick
 
+        local patch
         for patch in patches/*.patch; do
             git -C u-boot am "../$patch"
         done
-    elif [ "_$utag" != "_$(git -C u-boot branch --show-current)" ]; then
-        git -C u-boot checkout $utag
+    elif [ "$branch" != "$(git -C u-boot branch --show-current)" ]; then
+        git -C u-boot checkout "$branch"
     fi
 
-    rm -f idbloader*.img u-boot*.itb
-    if [ '_inc' != "_$1" ]; then
+    rm -f idbloader.img u-boot.itb
+    if ! is_param 'inc' "$@"; then
         make -C u-boot distclean
     fi
 
     # outputs: idbloader-r5c.img & u-boot-r5c.itb
     make -C u-boot nanopi-r5c-rk3568_defconfig
-    make -C u-boot -j$(nproc) BL31=$atf_file ROCKCHIP_TPL=$tpl_file
+    make -C u-boot -j$(nproc) BL31="$atf_file" ROCKCHIP_TPL="$tpl_file"
     cp u-boot/idbloader.img idbloader-r5c.img
     cp u-boot/u-boot.itb u-boot-r5c.itb
 
     # outputs: idbloader-r5s.img & u-boot-r5s.itb
     make -C u-boot nanopi-r5s-rk3568_defconfig
-    make -C u-boot -j$(nproc) BL31=$atf_file ROCKCHIP_TPL=$tpl_file
+    make -C u-boot -j$(nproc) BL31="$atf_file" ROCKCHIP_TPL="$tpl_file"
     cp u-boot/idbloader.img idbloader-r5s.img
     cp u-boot/u-boot.itb u-boot-r5s.itb
+
+    is_param 'cp' "$@" && cp_to_debian
 
     echo "\n${cya}idbloader and u-boot binaries are now ready${rst}"
     echo "\n${cya}copy nanopi r5c images to media:${rst}"
@@ -142,8 +149,20 @@ cherry_pick() {
     git -C u-boot cherry-pick 2cc6cde647e2cf61a29f389e8d263bf19672f0f5
 }
 
+cp_to_debian() {
+    local deb_dist=$(cat "../debian/make_debian_img.sh" | sed -n 's/\s*local deb_dist=.\([[:alpha:]]\+\)./\1/p')
+    [ -z "$deb_dist" ] && return
+    local cdir="../debian/cache.$deb_dist"
+    echo '\ncopying to debian cache...'
+    sudo mkdir -p "$cdir"
+    sudo cp -v './idbloader-r5c.img' "$cdir"
+    sudo cp -v './u-boot-r5c.itb' "$cdir"
+    sudo cp -v './idbloader-r5s.img' "$cdir"
+    sudo cp -v './u-boot-r5s.itb' "$cdir"
+}
+
 check_installed() {
-    local todo
+    local item todo
     for item in "$@"; do
         dpkg -l "$item" 2>/dev/null | grep -q "ii  $item" || todo="$todo $item"
     done
@@ -153,6 +172,18 @@ check_installed() {
         echo "   run: ${bld}${grn}sudo apt update && sudo apt -y install$todo${rst}\n"
         exit 1
     fi
+}
+
+is_param() {
+    local item match
+    for item in "$@"; do
+        if [ -z "$match" ]; then
+            match="$item"
+        elif [ "$match" = "$item" ]; then
+            return 0
+        fi
+    done
+    return 1
 }
 
 rst='\033[m'
@@ -165,5 +196,6 @@ mag='\033[35m'
 cya='\033[36m'
 h1="${blu}==>${rst} ${bld}"
 
-main $@
+cd "$(dirname "$(realpath "$0")")"
+main "$@"
 
